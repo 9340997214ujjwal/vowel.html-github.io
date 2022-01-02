20b33c5ab94f5adbb5619c3ab21e51874e785892
9340997214ujjwal <94543596+9340997214ujjwal@users.noreply.github.com>
2022-01-02 12:43:35 +0530

    uplode

---
+++ vowels.js
@@ -0,0 +1,48 @@
+alert("This program will check Wether the character is vowel or consonant")
+
+var char = prompt("Enter any charcter");
+
+if  (char == "A")
+{
+    alert( "Char is Vowel");
+}
+else if (char == "a")
+{
+    alert( "Char is Vowel");
+}
+else if (char == "E")
+{
+    alert( "Char is Vowel");
+}
+else if (char == "e")
+{
+    alert( "Char is Vowel");
+}
+else if (char == "I")
+{
+    alert( "Char is Vowel");
+}
+else if (char == "i")
+{
+    alert( "Char is Vowel");
+}
+else if (char == "O")
+{
+    alert( "Char is Vowel");
+}
+else if (char == "o")
+{
+    alert( "Char is Vowel");
+}
+else if (char == "U")
+{
+    alert( "Char is Vowel");
+}
+else if (char == "u")
+{
+    alert( "Char is Vowel");
+}
+else
+{
+    alert("it is a Consonant");
+}
